d15d87362b25f925df2215cb4ac54867d4f3f19c
Pramod Nurhakki <pramod@finflux.co>
2020-10-27 22:20:39 +0530

    FE-4437:Added Zoned date time codec

--- src/main/java/com/finflux/cassandra/core/CassandraSessionBuilder.java
+++ src/main/java/com/finflux/cassandra/core/CassandraSessionBuilder.java
@@ -14,6 +14,7 @@ import com.datastax.oss.driver.api.querybuilder.SchemaBuilder;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspace;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspaceStart;
 import com.datastax.oss.driver.internal.core.auth.PlainTextAuthProvider;
+import com.datastax.oss.driver.internal.core.type.codec.ZonedTimestampCodec;
 import com.finflux.cassandra.codec.LocalDateTimeCodec;
 import com.finflux.cassandra.util.ContactPointUtils;
 
@@ -57,6 +58,7 @@ public interface CassandraSessionBuilder {
         builder.withConfigLoader(driverConfigLoader);
         builder.withLocalDatacenter(cassandraConnectionData.getDataCenterName());
         builder.addTypeCodecs(new LocalDateTimeCodec());
+        builder.addTypeCodecs(new ZonedTimestampCodec());
         return builder;
     }
 
